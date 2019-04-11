619659e42f51e8dd0b01acac22ef417042f0e886
ashishkumar_bhavsar <ashishkumar_bhavsar@persistent.com>
2019-04-11 15:49:58 +0530

    Txt files are generated for YOLO

---
+++ gun detection python/ImageUtils/AumaticYoloGenerator.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+print(pyautogui.position())
+for i in range (1,2501):
+    pyautogui.click(71,523)
+    pyautogui.click(80, 403)
+    pyautogui.click(57, 258)
+
+#Point(x=71, y=523)
+#Point(x=80, y=403)
+#Point(x=57, y=258)
