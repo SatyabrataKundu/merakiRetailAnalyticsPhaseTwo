d745b319e27110ae1440f0ff6884bc9613f01982
Subhadip Jot <jotsubhadip@gmail.com>
2019-04-11 12:04:26 +0530

    Copy Paste Script

---
+++ FrontEnd/src/app/components/test-chart/test-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-chart',
+  templateUrl: './test-chart.component.html',
+  styleUrls: ['./test-chart.component.scss']
+})
+export class TestChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
