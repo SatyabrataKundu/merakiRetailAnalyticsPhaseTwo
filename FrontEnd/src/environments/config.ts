5a7ea6a64af323a7f4e5c0f437a5aa1f83af8391
Subhadip Jot <jotsubhadip@gmail.com>
2019-08-07 18:19:08 +0530

    local docker image config update for middleware

--- FrontEnd/src/environments/config.ts
+++ FrontEnd/src/environments/config.ts
@@ -2,5 +2,5 @@ export const config = {
     "countRefreshRate": 1000 * 10,
     "waitTimeRefreshRate": 1000 * 60,
     "notificationRefreshRate": 1000 * 60 * 10,
-    "ipAddress" : "http://10.53.18.238:4004"
+    "ipAddress" : "http://localhost:4004"
   };
